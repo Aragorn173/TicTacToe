7636da6a81ca5a96f1210b9af7e8da58c85ee2a3
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2022-02-21 11:13:11 +0100

    skapade spelplanen och började med hur spelet ska kunna köras

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -1,3 +1,21 @@
 public class tictactoe {
+    public static void main(String[] args) {
+        char[] board = new char[9];
 
+        for (int i = 0; i < 9; i++) {
+            board[i] = ' ';
+        }
+
+
+        System.out.println(board[0] + '|' + board[1] + '|' + board[2]);
+        System.out.println("-+-+-");
+        System.out.println(board[3] + '|' + board[4] + '|' + board[5]);
+        System.out.println("-+-+-");
+        System.out.println(board[6] + '|' + board[7] + '|' + board[8]);
+
+        if (board[0] == board[1] && board[1] == board[2]) {
+
+        }
+
+    }
 }
